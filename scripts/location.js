380758369aa0be4ca7f0050e2756e39de162d18d
Bobby Hughes <bobbyhughes6316@gmail.com>
2023-03-21 14:16:05 -0400

    fixed me mess ups

--- scripts/location.js
+++ scripts/location.js
@@ -8,14 +8,16 @@ document.addEventListener("change", (event) => {
     if (event.target.id === "location") {
         const selectedLocation = locationArr.find(location => location.id === parseInt(event.target.value));
         message = `You're ordering from ${selectedLocation.name}`;
-        document.querySelector('#location').textContent = message;
+        // document.querySelector('#location').textContent = message;
         setLocation(parseInt(event.target.value));
     }
 });
 
 export const location = () => {
-    let html = `<select id="location">
-    <option value="0">Store Location</option>`
+    let html = `
+    <select id="location">
+    <option value="0">Store Location</option>
+     `
     
     const listItemsArray = locationArr.map(location => {     
         
@@ -26,7 +28,10 @@ export const location = () => {
     
     html += listItemsArray.join("")
     html += "</select>"
+    html += `<div id="location-message"> </div>`
     html += message
       
+      
+
      return html
  }
--- scripts/yallHungry.js
+++ scripts/yallHungry.js
@@ -24,6 +24,7 @@ export const yallHungry = () => {
 
     <article class="locations">
       <h2> Store Location </h2>
+
         ${location()}
     </article>
 

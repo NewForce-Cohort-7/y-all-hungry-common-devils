87a6f5bf1cce7d9d7bc7d41fcc97c387dbddf70c
David Profitt <dlprofitt@gmail.com>
2023-03-22 09:19:03 -0400

    Added CSS, added header and imported font in index.html, adjusted html of yallHungry.js and main.js

--- scripts/yallHungry.js
+++ scripts/yallHungry.js
@@ -16,44 +16,56 @@ document.addEventListener(
     }
 )
 
+export const headerBannerImage = () => {
+    return `
+    <article class="imageContainer">
+        <img src="images/yallHungryIcon.png" alt="Y'all Hungry icon which has a cartoon hotdog wearing a trucker hat pointing to a food-truck that has the Y'all Hungry logo on top, as a sign" id="icon">
+    </article>
+
+    <article class="locations storeLocation">
+        <section id=" belowHeader">
+            <h2> Store Location </h2>
+            ${location()}
+        </section>
+    </article>
+    `
+}
 
 export const yallHungry = () => {
     return `
-    
-    <h1> Y'all Hungry?? </h1>
+        <article class="border">
+            <section class="belowHeaderLeft">
+                <article class="choices">
+                    <section class="choices__foods options">
+                        <h2> Food Items </h2>
+                        ${food()}
+                    </section>
+                    
+                    <section class="choices__drinks options">
+                        <h2> Drink Items </h2> 
+                        ${drinkOptions()}
+                    </section>
 
-    <article class="locations">
-      <h2> Store Location </h2>
-        ${location()}
-    </article>
-
-         <article class="choices">
-      <section class="choices__foods options">
-          <h2> Food Items </h2>
-          ${food()}
-      </section>
-        
-        <section class="choices__drinks options">
-        <h2> Drink Items </h2> 
-           ${drinkOptions()}
+                    <section class="choices__desserts options">
+                        <h2> Ice Cream Flavors </h2>
+                        ${dessert()}
+                    </section>
+                </article>
             </section>
 
-        <section class="choices__desserts options">
-        <h2> Ice Cream Flavors </h2>
-           ${dessert()}
+            <section class="belowHeaderCenter">
+                <article class="customOrders">
+                    <h2> Orders </h2> 
+                    ${drankOrder}
+                    ${orders()}
+                </article>
             </section>
 
-        <article>
-
-            <button id="orderButton"> Place Order </button>
-             
+            <section class="belowHeaderRight">
+                <article class="button">
+                    <button id="orderButton"> Place Order </button>
+                </article>
+            </section>
         </article>
-
-        <article class="customOrders">
-            <h2> Orders </h2> 
-            ${drankOrder}
-            ${orders()}
-        </article>
-    
     `
 }
